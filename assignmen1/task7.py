829e637ea8e4d3228627a73fe0262ab2b88ec055
CS5300 <jarchib2@uccs.edu>
2025-09-21 18:02:29 +0000

    updated tests and program functionality to run efficently and effectively

--- assignmen1/task7.py
+++ assignmen1/task7.py
@@ -3,20 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 
-def load_data():
-    # Local synthetic data: [hours studied, hours slept]
-    X = [
-        [2, 5],
-        [3, 6],
-        [4, 4],
-        [5, 5],
-        [6, 6],
-        [7, 7],
-        [8, 5],
-        [9, 6]
-    ]
-    y = [50, 55, 60, 65, 70, 75, 80, 85]
-    return X, Y
+#Sets up and trains a linear regression model from scikit-Learn
 
 #Pre processes the data by scaling/ normalizing the points
 def preprocess(X_train, X_test):
@@ -37,4 +24,4 @@ def evaluate_model(model, X_test, Y_test):
     mse = mean_squared_error(Y_test, Y_pred)
     print("Predictions:", Y_pred)
     print("Mean Squared Error:", mse)
-    return mse
+    return mse, Y_pred
--- assignmen1/task7Test.py
+++ assignmen1/task7Test.py
@@ -1,3 +1,5 @@
+
+import task7
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
@@ -14,26 +16,16 @@ X = [
         ]
 Y = [50, 55, 60, 65, 70, 75, 80, 85]
 
-def test_load_data():
-    reg = SimpleRegressor()
-    X, Y = reg.load_data()
-    assert len(X) == len(y) == 8
-    assert isinstance(X[0], list)
-
 def test_preprocess():
-    reg = SimpleRegressor()
-    # X, y = reg.load_data()
     X_train, X_test, _, _ = train_test_split(X, Y, test_size=0.25, random_state=42)
-    X_train_scaled, X_test_scaled = reg.preprocess(X_train, X_test)
+    X_train_scaled, X_test_scaled = task7.preprocess(X_train, X_test)
     assert len(X_train_scaled) == len(X_train)
     assert len(X_test_scaled) == len(X_test)
 
 def test_train_and_evaluate():
-    reg = SimpleRegressor()
-    X, Y = reg.load_data()
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=42)
-    X_train_scaled, X_test_scaled = reg.preprocess(X_train, X_test)
-    reg.train(X_train_scaled, Y_train)
-    mse, Y_pred = reg.evaluate(X_test_scaled, Y_test)
+    X_train, X_test, Y_train, Y_test = task7.train_test_split(X, Y, test_size=0.25, random_state=42)
+    X_train_scaled, X_test_scaled = task7.preprocess(X_train, X_test)
+    model = task7.train_model(X_train_scaled, Y_train)
+    mse, Y_pred = task7.evaluate_model(model, X_test_scaled, Y_test)
     assert mse >= 0
     assert len(Y_pred) == len(Y_test)
